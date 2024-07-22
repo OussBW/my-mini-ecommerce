ec04495975bab3ead1ee4d1f46b0a039682ac97c
Oussama <ouss.bw@gmail.com>
2024-07-22 13:36:48 +0200

    Bonus: add sorting feature

--- src/components/organisms/ProductList.vue
+++ src/components/organisms/ProductList.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="card">
-    <DataView :value="products" :layout="layout" paginator :rows="rows.value">
+    <DataView
+      :value="products"
+      :layout="layout"
+      paginator
+      :rows="rows.value"
+      :sortOrder="sortOrder"
+      :sortField="sortField"
+    >
       <template #header>
         <div class="flex justify-between">
-          <Select
-            v-model="rows"
-            :options="rowsOptions"
-            optionLabel="label"
-            placeholder="Items per page"
-          />
+          <div class="flex justify-between">
+            <Select
+              v-model="rows"
+              :options="rowsOptions"
+              optionLabel="label"
+              placeholder="Items per page"
+            />
+            <Select
+              v-model="sortKey"
+              :options="sortOptions"
+              optionLabel="label"
+              placeholder="Sort By"
+              class="ml-2"
+              @change="onSortChange($event)"
+            />
+          </div>
           <SelectButton v-model="layout" :options="options" :allowEmpty="false">
             <template #option="{ option }">
               <i :class="[option === 'list' ? 'pi pi-bars' : 'pi pi-table']" />
@@ -63,12 +80,37 @@ const { loadProducts } = useProductStore();
 const loading = ref(true);
 const layout = ref("grid");
 const options = ref(["list", "grid"]);
+const sortOptions = ref([
+  { label: "Price High to Low", value: "!price" },
+  { label: "Price Low to High", value: "price" },
+  { label: "Rating High to Low", value: "!rating.rate" },
+  { label: "Rating Low to High", value: "rating.rate" },
+  { label: "Category", value: "category" },
+]);
+const sortKey = ref();
+const sortOrder = ref();
+const sortField = ref();
 const rowsOptions = ref([
   { label: "3", value: 3 },
   { label: "6", value: 6 },
   { label: "12", value: 12 },
 ]);
 const rows = ref({ value: 5 });
+
+const onSortChange = (event) => {
+  const value = event.value.value;
+  const sortValue = event.value;
+
+  if (value.indexOf("!") === 0) {
+    sortOrder.value = -1;
+    sortField.value = value.substring(1, value.length);
+    sortKey.value = sortValue;
+  } else {
+    sortOrder.value = 1;
+    sortField.value = value;
+    sortKey.value = sortValue;
+  }
+};
 
 onMounted(() => {
   loadProducts().then(() => {
